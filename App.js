4d2c0a2512cdfbec27af6133e9dd0f3e381a3306
nourhan hossam <83311419+nourhan872@users.noreply.github.com>
2023-05-07 01:23:06 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ MenuList.js
@@ -0,0 +1,39 @@
+import Mango from "./assets/mango.jpg";
+import Watermelon from "./assets/watermelon.jpg";
+import Caramel from "./assets/caramel.jpg";
+import Chocolate from "./assets/chocolate.jpg";
+import Mixed from "./assets/mixed.jpg";
+import Lemon from "./assets/lemon.jpg";
+
+export const MenuList = [
+  {
+    name: "Mango Smoothie",
+    image: Mango,
+    price: 19.99,
+  },
+  {
+    name: "Melon Smoothie",
+    image: Watermelon,
+    price: 14.99,
+  },
+  {
+    name: "Caramel Smoothie",
+    image: Caramel,
+    price: 29.53,
+  },
+  {
+    name: "Chocolate Smoothie",
+    image: Chocolate,
+    price: 24.99,
+  },
+  {
+    name: "Berry Smoothie",
+    image: Mixed,
+    price: 14.99,
+  },
+  {
+    name: "Green Smoothie",
+    image: Lemon,
+    price: 16.99,
+  },
+];
